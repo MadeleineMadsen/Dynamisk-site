65a7859cc248b670ed188367aa7736063fa22dc4
Madeleine Madsen <mama0002@stud.kea.dk>
2023-08-28 13:34:46 +0200

    produktliste

---
+++ produktliste.js
@@ -0,0 +1,24 @@
+fetch("https://kea-alt-del.dk/t7/api/products")
+  .then((res) => res.json())
+  .then((data) => showProducts(data));
+
+function showProducts(products) {
+  // looper og kalder showProduct
+  products.forEach(showProduct);
+}
+
+function showProduct(product) {
+  //   console.log(product);
+  //Fang template
+  const template = document.querySelector("#smallProductTemplate").content;
+  //Lav en kopi
+  const copy = template.cloneNode(true);
+  //Ændre indhold
+  copy.querySelector("h3").textContent = product.productdisplayname;
+  if (product.soldout) {
+    //produktet er udsolgt
+    copy.querySelector("article").classList.add("soldOut");
+  }
+  //Appende
+  document.querySelector("main").appendChild(copy);
+}
