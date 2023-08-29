30deb4dcda67bab34edbbf8b971a261e0af9ae44
Madeleine Madsen <mama0002@stud.kea.dk>
2023-08-29 14:51:52 +0200

    produkt 2

--- js/produkt.js
+++ js/produkt.js
@@ -1,4 +1,7 @@
-fetch("https://kea-alt-del.dk/t7/api/products/1525")
+const urlParams = new URLSearchParams (window.location.search);
+const id = urlParams.get("id");
+ 
+fetch("https://kea-alt-del.dk/t7/api/products/" + id)
   .then((response) => response.json())
   .then((data) => showProduct(data));
 
@@ -7,7 +10,9 @@ function showProduct(product) {
   document.querySelector(".purchaseBox h3").textContent =
     product.productdisplayname;
   document.querySelector(".purchaseBox .brand").textContent = product.brandname;
-  document.querySelector("img").src=`https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`
+  document.querySelector(
+    "img"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
 }
 
 // id	1525
